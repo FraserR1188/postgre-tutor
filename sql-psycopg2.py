07b4d99d82aeafed8e57897e434467c41e7dd781
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2025-01-16 06:35:49 +0000

    Pyco add and sqlalchmey

---
+++ sql-psycopg2.py
@@ -0,0 +1,48 @@
+import psycopg2
+
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only be "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only be the albums from "ArtistId" #51 from the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Challenge
+# cursor.execute('SELECT * FROM "Artist"')
+
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Cake"])
+
+# cursor.execute('SELECT * FROM "Track" WHERE "Name" = %s', ["Cake"])
+
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["test"])
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
